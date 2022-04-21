3a9bfc8fd55a26224fd2b33d851b1c6830c48906
Artem <voroshilin777@icloud.com>
2022-04-21 06:35:47 +0300

    Initial commit

---
+++ src/main/java/Programm.java
@@ -0,0 +1,7 @@
+public class Programm {
+    public static void main(String[] args) {
+        Numbers numbers=new Numbers();
+        int sum=numbers.sum(5);
+        System.out.println("Sum from 0 to 5="+sum);
+    }
+}
---
+++ src/test/java/NumbersTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumbersTest {
+
+    @Test
+    void sum() {
+        Numbers numbers=new Numbers();
+        int actual= numbers.sum(5);
+        int expected=15;
+        assertEquals(expected,actual);
+    }
+}
